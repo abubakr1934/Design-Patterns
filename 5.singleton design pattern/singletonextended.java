3d4d2ccce31c6207c9fc62f26b95f19eb4bf3356
Mohammad Abubakr Khanooni <115393179+abubakr1934@users.noreply.github.com>
2023-09-17 13:01:38 +0530

    Update singletonextended.java

--- 5.singleton design pattern/singletonextended.java
+++ 5.singleton design pattern/singletonextended.java
@@ -1,5 +1,5 @@
 public class singletonextended extends Singleton{
-    static singletonextended s10=new singletonextended(7);
+    private final static singletonextended s10=new singletonextended(7);
     int a;
     public singletonextended(int a)
     {
